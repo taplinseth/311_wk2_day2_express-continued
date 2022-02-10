7e7633f6df12e86016c22481df2d892e2467c0d6
taplinseth <taplinseth@gmail.com>
2022-02-10 12:10:43 -0600

    routing and controllers

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const contacts = require("/data/contacts");
-const vehicles = require("/data/vehicles");
-const comments = require("/data/comments");
-const products = require("/data/products");
-
+const contacts = require('./routes/contacts');
+const vehicles = require('./routes/vehicles');
+const comments = require('./routes/comments');
+const products = require('./routes/products');
 
 app.use(express.static('public'));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(contacts);
+app.use(vehicles);
+app.use(comments);
+app.use(products);
 
 
 app.listen(port, () => {
