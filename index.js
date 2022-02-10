c3fd82077c7e36e20a456d2a494561f71b9a55c8
taplinseth <taplinseth@gmail.com>
2022-02-10 11:54:49 -0600

    files

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const port = process.env.PORT || 4000;
 
-const port = process.env.PORT || 4001;
+const contacts = require("/data/contacts");
+const vehicles = require("/data/vehicles");
+const comments = require("/data/comments");
+const products = require("/data/products");
+
+
+app.use(express.static('public'));
+app.use(bodyParser.json())
+
 
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
